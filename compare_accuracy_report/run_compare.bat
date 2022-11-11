0d93d7f05951cad8e95677afda07c3b60729e4a2
xipingyan <xiping.yan@intel.com>
2022-11-11 10:05:27 +0800

    add run_compare.bat

---
+++ compare_accuracy_report/run_compare.bat
@@ -0,0 +1,4 @@
+set ver1="..\\..\\report_master_int8_spr_models\accuracy.cpu.20221108_10_49.csv"
+set ver2="..\\..\\report_rm_patch_int8_spr_models\accuracy.cpu.20221110_14_28.csv"
+
+python.exe %ver1% %ver2% 0.01
